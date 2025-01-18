98b98a38530dd9dcc8921ec705c34e084499607a
dchacon <dchacon@payvalida.com>
2025-01-18 14:32:47 -0500

    Fix ci/cd

--- modules/lambda_api_module/lambda_code/app.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": f"Environment Variable: {os.getenv('ENV_VAR')}"
-    }
--- modules/lambda_api_module/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "api_gateway_url" {
-  description = "URL of the API Gateway"
-  value       = aws_api_gateway_rest_api.api.name
-}
-
-output "waf_arn" {
-  description = "ARN of the WAF ACL"
-  value       = aws_wafv2_web_acl.waf.arn
-}
